bb26a22f308523186b9e0c4f98678b41f3af71d6
Will Charczuk <will@blend.com>
2019-03-09 19:14:31 -0800

    updates

---
+++ README.md
@@ -0,0 +1,4 @@
+skynet
+======
+
+This is my repo for terraform config for my personal k8s cluster.
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  credentials = "${file("account.json")}"
-  project     = "${var.gcloud.project}"
-  region      = "us-central1"
-}
